ef8380515f9d1a06a78ec42c0ba5d7fdd2040c4b
Alexander Schwarz <schwarz@afpkoeln.onmicrosoft.com>
2024-10-31 11:45:18 +0100

    slider btn

---
+++ website/scripts/script.js
@@ -0,0 +1,9 @@
+const prevBtn = document.getElementsByClassName("slider-img");
+
+
+    for(i= 0; i< prevBtn.length; i++){
+        prevBtn[i].classList.add("active");
+        console.log(prevBtn[i]);
+        
+    }
+
