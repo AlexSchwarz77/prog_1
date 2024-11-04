29a737e32c406bfd9535c52ef6e63a53d8fe0b34
Alexander Schwarz <schwarz@afpkoeln.onmicrosoft.com>
2024-11-04 09:52:29 +0100

    slider fix

--- website/scripts/script.js
+++ website/scripts/script.js
@@ -1,23 +1,36 @@
-const prevBtn = document.querySelector(".prevBtn");
-const img = document.querySelectorAll(".slider-img")
-console.log(img);
+let prevBtn = document.querySelector(".prevBtn");
+let nextBtn = document.querySelector(".nextBtn");
+let img = document.querySelectorAll(".slider-img");
 let index = 0;
 
- prevBtn.addEventListener("click",  //(e) => {
-    
-    prevImg
-    
-// }
-) 
+prevBtn.addEventListener("click", prevImg);
+nextBtn.addEventListener("click", nextImg);
 
-
-function prevImg(){
-    document.querySelector('.slider-img.active').classList.remove('active');
-    let i   = img[3]
-    i.classList.add("active");
-    img.prepend(i)
-    console.log(img);
-    
-
-
+function prevImg() {
+  for (i = 0; i < img.length; i++) {
+    if (img[i].classList.contains("active")) {
+      index = i - 1;
+      img[i].classList.remove("active");
+    }
+  }
+  if (index < img.length && index > -1) {
+    img[index].classList.add("active");
+  } else {
+    index = img.length - 1;
+    img[index].classList.add("active");
+  }
+}
+function nextImg() {
+  for (i = 0; i < img.length; i++) {
+    if (img[i].classList.contains("active")) {
+      index = i + 1;
+      img[i].classList.remove("active");
+    }
+  }
+  if (index < img.length ) {
+    img[index].classList.add("active");
+  } else {
+    index = 0;
+    img[index].classList.add("active");
+  }
 }
